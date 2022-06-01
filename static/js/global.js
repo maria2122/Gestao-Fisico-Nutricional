4ba416cee74c66364b3e8e82ca460b04626c4071
maria2122 <mariajsantos199@gmail.com>
2022-06-01 13:23:27 -0300

    Criação de Função de Carregamento de Dados da Tabela para o Formulário

--- static/js/global.js
+++ static/js/global.js
@@ -1,30 +1,18 @@
-function editarAlimentoTabela() {
-    var id = this.parentNode.parentNode.id;
-    var nome = document.getElementById(id).getElementsByTagName('td')[0].innerHTML;
-    var calorias = document.getElementById(id).getElementsByTagName('td')[1].innerHTML;
-    var proteina = document.getElementById(id).getElementsByTagName('td')[2].innerHTML;
-    var carboidrato = document.getElementById(id).getElementsByTagName('td')[3].innerHTML;
-    var gordura = document.getElementById(id).getElementsByTagName('td')[4].innerHTML;
-    
-    var form = document.getElementById('form-editar-alimento');
-    form.nome.value = nome;
-    form.calorias.value = calorias;
-    form.proteina.value = proteina;
-    form.carboidrato.value = carboidrato;
-    form.gordura.value = gordura;
-    
-    form.id.value = id;
-    
-    document.getElementById('form').style.display = 'block';
+//Editar elementos da tabela
+function carregaDadosDaTabelaParaFormAlimento(td){
+    selecaodaLinha = td.parentElement.parentElement;
+    document.getElementById("codigo").value = selecaodaLinha.cells[0].innerHTML;
+    document.getElementById("nome").value = selecaodaLinha.cells[1].innerHTML;
+    document.getElementById("descricao").value = selecaodaLinha.cells[2].innerHTML;
+    document.getElementById("valor_calorico").value = selecaodaLinha.cells[3].innerHTML;
+    document.getElementById("valor_gordura").value = selecaodaLinha.cells[4].innerHTML;
+    document.getElementById("valor_proteina").value = selecaodaLinha.cells[5].innerHTML;
+    document.getElementById("valor_carboidrato").value = selecaodaLinha.cells[6].innerHTML;
+    document.getElementById("ativo").value = selecaodaLinha.cells[7].innerHTML;
 }
+function editarCardapio(){
 
-function preencheForm() {
-    var form = document.getElementById('form-editar-alimento');
-    form.nome.value = this.parentNode.parentNode.getElementsByTagName('td')[0].innerHTML;
-    form.calorias.value = this.parentNode.parentNode.getElementsByTagName('td')[1].innerHTML;
-    form.proteina.value = this.parentNode.parentNode.getElementsByTagName('td')[2].innerHTML;
-    form.carboidrato.value = this.parentNode.parentNode.getElementsByTagName('td')[3].innerHTML;
-    form.gordura.value = this.parentNode.parentNode.getElementsByTagName('td')[4].innerHTML;
-    
-    form.id.value = this.parentNode.parentNode.id;
+}
+function editarAtividadeFisica(){
+
 }
