a4f6ebf0b16ea703405290898239ab488459eb49
maria2122 <mariajsantos199@gmail.com>
2022-06-25 10:20:56 -0300

    Termino dos INSERTs Tabelas(usuario, cardapio, alimento_cardapio, ficha, atividade_fisica_ficha) ok

--- static/0 - (MYSQL) (Versão Final) (Entrega Banco de dados II).sql
+++ static/0 - (MYSQL) (Versão Final) (Entrega Banco de dados II).sql
@@ -127,34 +127,7 @@ VALUES (5, "Filé de frango 100g","Filé de frango 100g",101,0,0,0,1);
 INSERT INTO alimento(id, nome, descricao, valor_calorico, valor_gordura, valor_proteina, valor_carboidrato, ativo) 
 VALUES (6, "Filé-mignon 100g","Filé-mignon 100g",140,0,0,0,1);
 
--- -----------------------------------------------------------------------------
--- Cardapio
--- -----------------------------------------------------------------------------
-/*INSERT INTO cardapio(id, data_inicio, data_fim) 
-VALUES (1, "2022-03-21", "2022-04-30",  **COLOCAR UM USUÁRIO);*/
-
--- -----------------------------------------------------------------------------
--- Alimento_Cardapio
--- -----------------------------------------------------------------------------
--- Café com açúcar 50ml
-/* **EXECUTAR DEPOIS DE CRIAR AO MENOS UM CARDAPIO PARA LIGAR 
-INSERT INTO alimento_cardapio(id, alimento_id, cardapio_id, quantidade) 
-VALUES (1,1,1,3);
--- Café sem açúcar 40ml
-INSERT INTO alimento_cardapio(id, alimento_id, cardapio_id, quantidade) 
-VALUES (2,2,1,5);
--- Refrigerante de cola 350ml
-INSERT INTO alimento_cardapio(id, alimento_id, cardapio_id, quantidade) 
-VALUES (3,3,1,5);
--- Cupim 100g
-INSERT INTO alimento_cardapio(id, alimento_id, cardapio_id, quantidade) 
-VALUES (4,4,1,5);
--- Filé de frango 100g
-INSERT INTO alimento_cardapio(id, alimento_id, cardapio_id, quantidade) 
-VALUES (5,5,1,5);
--- Filé-mignon 100g
-INSERT INTO alimento_cardapio(id, alimento_id, cardapio_id, quantidade) 
-VALUES (6,6,1,5); */
+select * from alimento;
 
 -- -----------------------------------------------------------------------------
 -- Atividade_Fisica
@@ -171,3 +144,106 @@ INSERT INTO atividade_fisica(id, nome, descricao, gasto_calorico, ativo)
 VALUES (5,"Dormir 1Hr","Dormir por uma hora", 60, 1);
 INSERT INTO atividade_fisica(id, nome, descricao, gasto_calorico, ativo) 
 VALUES (6,"Limpeza 1Hr","Limpeza de casa por uma hora", 60, 1);
+
+select * from atividade_fisica;
+-- -----------------------------------------------------------------------------
+-- Usuario
+-- -----------------------------------------------------------------------------
+
+INSERT INTO usuario(id, nome, login, senha, cliente, profissional_ed_fisica, nutricionista, administrador)
+VALUES(1,"Marcio Augusto dos Santos","marcio.augusto","123", 1, 1, 1, 1);
+
+INSERT INTO usuario(id, nome, login, senha, cliente, profissional_ed_fisica, nutricionista, administrador)
+VALUES(2,"Vanessa Cristina Pallos","vanessa.cristina","123", 0, 0, 1, 0);
+
+INSERT INTO usuario(id, nome, login, senha, cliente, profissional_ed_fisica, nutricionista, administrador)
+VALUES(3,"Elizabeth Madeira","elizabeth.madeira","123", 0, 1, 0, 0);
+
+INSERT INTO usuario(id, nome, login, senha, cliente, profissional_ed_fisica, nutricionista, administrador)
+VALUES(4,"Antonio Carlos Silva","antonio.carlos","123", 1, 0, 0, 0);
+
+select * from usuario;
+
+-- -----------------------------------------------------------------------------
+-- Cardapio
+-- -----------------------------------------------------------------------------
+-- Usuário 4 - Cliente (em todos os cardápios)
+
+INSERT INTO cardapio(id, data_inicio, data_fim, usuario_consumidor_id) 
+VALUES (1, "2022-03-21", "2022-04-21",  4);
+
+INSERT INTO cardapio(id, data_inicio, data_fim, usuario_consumidor_id) 
+VALUES (2, "2022-04-21", "2022-05-21",  4);
+
+INSERT INTO cardapio(id, data_inicio, data_fim, usuario_consumidor_id) 
+VALUES (3, "2022-05-21", "2022-06-21",  4);
+
+INSERT INTO cardapio(id, data_inicio, data_fim, usuario_consumidor_id) 
+VALUES (4, "2022-06-21", "2022-07-21",  4);
+
+select * from cardapio;
+
+-- -----------------------------------------------------------------------------
+-- Alimento_Cardapio -- Cardinalidade N:N(tbls Alimento:Cardapio)
+-- -----------------------------------------------------------------------------
+-- Café com açúcar 50ml
+INSERT INTO alimento_cardapio(id, alimento_id, cardapio_id, quantidade) 
+VALUES (1,1,1,3);
+-- Café sem açúcar 40ml
+INSERT INTO alimento_cardapio(id, alimento_id, cardapio_id, quantidade) 
+VALUES (2,2,1,5);
+-- Refrigerante de cola 350ml
+INSERT INTO alimento_cardapio(id, alimento_id, cardapio_id, quantidade) 
+VALUES (3,3,2,5);
+-- Cupim 100g
+INSERT INTO alimento_cardapio(id, alimento_id, cardapio_id, quantidade) 
+VALUES (4,4,3,5);
+-- Filé de frango 100g
+INSERT INTO alimento_cardapio(id, alimento_id, cardapio_id, quantidade) 
+VALUES (5,5,3,5);
+-- Filé-mignon 100g
+INSERT INTO alimento_cardapio(id, alimento_id, cardapio_id, quantidade) 
+VALUES (6,6,4,5); 
+
+select * from alimento_cardapio;
+
+-- -----------------------------------------------------------------------------
+-- Ficha_Atividade - Usuário 4 Cliente que seguirá o mesmo período do cardápio
+-- -----------------------------------------------------------------------------
+INSERT INTO ficha_atividade(id, data_inicio, data_fim, usuario_praticante_id)
+VALUES (1, "2022-03-21", "2022-04-21",  4);
+
+INSERT INTO ficha_atividade(id, data_inicio, data_fim, usuario_praticante_id)
+VALUES (2, "2022-04-21", "2022-05-21",  4);
+
+INSERT INTO ficha_atividade(id, data_inicio, data_fim, usuario_praticante_id)
+VALUES (3, "2022-05-21", "2022-06-21",  4);
+
+INSERT INTO ficha_atividade(id, data_inicio, data_fim, usuario_praticante_id)
+VALUES (4, "2022-06-21", "2022-07-21",  4);
+
+select * from ficha_atividade;
+
+-- -----------------------------------------------------------------------------
+-- Atividade_Fisica_Ficha -- Cardinalidade N:N(tbls Atividade_Fisica:Ficha)
+-- -----------------------------------------------------------------------------
+
+INSERT INTO atividade_fisica_ficha(id, atividade_fisica_id, ficha_atividade_id, serie, repeticao, tempo_descanso_segundos)
+VALUES(1, 1, 2, 15,5,600);
+
+INSERT INTO atividade_fisica_ficha(id, atividade_fisica_id, ficha_atividade_id, serie, repeticao, tempo_descanso_segundos)
+VALUES(2, 2, 1, 15,5,900);
+
+INSERT INTO atividade_fisica_ficha(id, atividade_fisica_id, ficha_atividade_id, serie, repeticao, tempo_descanso_segundos)
+VALUES(3, 3, 3, 30,5,300);
+
+INSERT INTO atividade_fisica_ficha(id, atividade_fisica_id, ficha_atividade_id, serie, repeticao, tempo_descanso_segundos)
+VALUES(4, 4, 3, 30,5,900);
+
+INSERT INTO atividade_fisica_ficha(id, atividade_fisica_id, ficha_atividade_id, serie, repeticao, tempo_descanso_segundos)
+VALUES(5, 5, 4, 60,5,300);
+
+INSERT INTO atividade_fisica_ficha(id, atividade_fisica_id, ficha_atividade_id, serie, repeticao, tempo_descanso_segundos)
+VALUES(6, 6, 1, 60,5,600);
+
+select * from atividade_fisica_ficha;
